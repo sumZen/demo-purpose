6bfce19631b49b0c956f5327143f185873389d5a
sumZen <sumantakmohanta@gmail.com>
2025-07-04 19:40:07 +0530

    Add Promises demo

--- script.js
+++ script.js
@@ -1,1 +1,12 @@
 console.log("Hi My Javascript journey has started.");
+
+async function delayPrint  () {
+return 'Hi how are you';
+};
+
+delayPrint().then((data)=>{
+    console.log(data);
+})
+.catch((err)=>{
+    if (err) throw err;
+});
